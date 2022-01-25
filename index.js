f5462e9f6adc80065d3306d46040fce7eec5e852
HopeMashal <mashal.hope@gmail.com>
2022-01-25 10:49:41 +0200

    edit

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.use(express.json());
 app.use("/auth", require("./routes/auth"));
 app.use("/posts", require("./routes/posts"));
 
-app.listen(5000, () => {
+app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
