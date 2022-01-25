0e3a3ba48131e24bd696608fce791613f3c61a1f
HopeMashal <mashal.hope@gmail.com>
2022-01-25 17:12:15 +0200

    edit client

--- routes/auth.js
+++ routes/auth.js
@@ -62,7 +62,7 @@ router.post(
       { email },
       process.env.ACCESS_TOKEN_SECRET,
       {
-        expiresIn: "10s",
+        expiresIn: "1m",
       }
     );
 
@@ -117,18 +117,92 @@ router.post("/login", async (req, res) => {
     });
   }
 
-  // Send JWT
+  // Send JWT access token
   const accessToken = await JWT.sign(
     { email },
     process.env.ACCESS_TOKEN_SECRET,
     {
-      expiresIn: "10s",
+      expiresIn: "1m",
     }
   );
 
+  // Refresh token
+  const refreshToken = await JWT.sign(
+    { email },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: "5m",
+    }
+  );
+
+  // Set refersh token in refreshTokens array
+  refreshTokens.push(refreshToken);
+
   res.json({
     accessToken,
+    refreshToken,
   });
+});
+
+let refreshTokens = [];
+
+// Create new access token from refresh token
+router.post("/token", async (req, res) => {
+  const refreshToken = req.header("x-auth-token");
+
+  // If token is not provided, send error message
+  if (!refreshToken) {
+    res.status(401).json({
+      errors: [
+        {
+          msg: "Token not found",
+        },
+      ],
+    });
+  }
+
+  // If token does not exist, send error message
+  if (!refreshTokens.includes(refreshToken)) {
+    res.status(403).json({
+      errors: [
+        {
+          msg: "Invalid refresh token",
+        },
+      ],
+    });
+  }
+
+  try {
+    const user = await JWT.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    // user = { email: 'hope@gmail.com', iat: ..., exp: ... }
+    const { email } = user;
+    const accessToken = await JWT.sign(
+      { email },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+    res.json({ accessToken });
+  } catch (error) {
+    res.status(403).json({
+      errors: [
+        {
+          msg: "Invalid token",
+        },
+      ],
+    });
+  }
+});
+
+// Deauthenticate - log out
+// Delete refresh token
+router.delete("/logout", (req, res) => {
+  const refreshToken = req.header("x-auth-token");
+
+  refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+  res.sendStatus(204);
 });
 
 module.exports = router;
